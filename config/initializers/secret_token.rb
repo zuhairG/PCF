e12fc0b37d807d172bdebd15cd069949527a96d3
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2013-09-09 12:57:22 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PCF::Application.config.secret_token = 'c6f45f53f90383b40764b19ddfbaaf2afe72f3b565e7d93bed8c914b2cd68d83066dee33462ae21cb00623815297586685e8fd8d0a3b2c58174ac2f83ffd4d47'
