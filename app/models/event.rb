e928c379a88f4791b885ec1afed4ffe4d4746164
sheila <sheilaaachan@gmail.com>
2013-10-09 17:22:38 -0400

    added event scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,11 +4,17 @@ class Event < ActiveRecord::Base
   has_many :acts
   default_scope order('date, start_time ASC')
 
+  scope :today, where("date = ?", Date.current)
+  scope :current, today.where("? BETWEEN start_time AND end_time",Time.now)
+  scope :past, where("date < ?", Date.current)
+  scope :for_date, lambda {|date| where("date = ?", date) }
+  scope :for_venue, lambda {|venue| where("venue_id = ?", venue) }
+
 
 private 
 
   def venue_name(id)
     name = Venue.find(id)
     return name
-  end  
+  end
 end
