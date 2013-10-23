aced907638bd549d48026c15229ada0c72c793aa
sheila <sheilaaachan@gmail.com>
2013-10-23 13:53:50 -0400

    updated look of schedule

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20131011165530) do
     t.date     "date"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "actslist"
     t.integer  "act_id"
   end
 
