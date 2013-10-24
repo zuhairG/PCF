957e62445dfd8dcafa9f5dbb4a74cd9af43c1be8
sheila <sheilaaachan@gmail.com>
2013-10-23 22:18:04 -0400

    fixed navbar

--- app/assets/javascripts/bootstrap-overrides.js
+++ app/assets/javascripts/bootstrap-overrides.js
@@ -1,3 +1,19 @@
 $("#star").click(function() {
   $("#star").html('<%= image_tag("star_filled.png", :height => 20)%>')
 });
+
+$('#nav-bar').hide();
+   
+$(window).on('scroll',function() {
+  var scrolltop = $(this).scrollTop();
+
+  if(scrolltop >= 50) {
+    $('#nav-bar').fadeIn(250);
+    $('.schedule-nav-contain').addClass('fixed container');
+  }
+   
+  else if(scrolltop <= 45) {
+    $('#nav-bar').fadeOut(250);
+    $('.schedule-nav-contain').removeClass('fixed container');
+  }
+});
