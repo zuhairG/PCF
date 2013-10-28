7cbae4d5cdbf3b576acb9fba5eeb6d1a001ff339
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2013-10-28 16:56:28 -0400

    venue id is now accessible

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,5 @@
 class Venue < ActiveRecord::Base
-  attr_accessible :city, :name, :state, :street_address, :zip_code, :event_id
+  attr_accessible :city, :name, :state, :street_address, :zip_code, :event_id, :venue_id
   belongs_to :event
   
   validates_presence_of :city, :state, :name, :street_address, :zip_code, :event_id
