0a05de332bc40fd0d21cbb2d911e99a95b95083c
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2013-10-30 13:35:35 -0400

    attr accessible added to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :name
   # attr_accessible :title, :body
   
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
