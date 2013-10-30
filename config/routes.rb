c5e73695203fede24e74a5ce865cf44ff6f3ca10
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2013-10-30 12:33:30 -0400

    heroku routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 PCF::Application.routes.draw do
-  devise_for :user, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+
   devise_scope :user do
     get "users/sign_in", :to => "devise/sessions#new"
     get "users/sign_out", :to => "devise/sessions#destroy"
