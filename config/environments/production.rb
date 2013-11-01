80bf61f8cd7294f09f8a1bd57e37a3aa262ded8c
sheila <sheilaaachan@gmail.com>
2013-10-31 23:42:30 -0400

    restructured stylesheets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ PCF::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application.css application-desktop.css application-mobile.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
