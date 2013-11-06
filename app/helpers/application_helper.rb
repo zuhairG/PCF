ab6f77c799dd8608c9dac0b7f87ea09d67324dba
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2013-11-06 14:30:35 -0500

    login changed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
 	def mobile_user_agent?
 	  request.env["HTTP_USER_AGENT"].try :match, /(iphone|ipod|android)/i
 	end
+	
+	def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  
 end
