8d8b65b7f4c6c5b9263b760375b594e380a60bc9
sennap <sennaparsa@gmail.com>
2013-11-18 15:56:28 -0500

    tests passing for performers performer_acts and events

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -3,5 +3,6 @@ class Performer < ActiveRecord::Base
   has_many :performer_acts
   has_many :acts, through: :performer_acts
   validates_presence_of :bio, :email, :name, :twitter
-  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  validates_format_of :email, :with => /^[\w]([^@\s,;]+)@(([a-z0-9.-]+\.)+(com|edu|org|net|gov|mil|biz|info))$/i
+  validates_format_of :twitter, :with => /^[-\w\._@]+$/i, :message => "Username should only contain letters, numbers, or .-_@"
 end
