13b2440a25f25e9358984aae70d078d1ed3f5068
sheila <sheilaaachan@gmail.com>
2013-11-20 11:41:56 -0500

    act contain edited

--- app/assets/javascripts/bootstrap-overrides.js
+++ app/assets/javascripts/bootstrap-overrides.js
@@ -2,6 +2,22 @@ $("#star").click(function() {
   $("#star").html('<%= image_tag("assets/star_filled.png", :height => 20)%>')
 });
 
+// $(window).scroll(
+// 	function (event) {
+// 		var y = $(this).scrollTop();
+// 		if y<0{
+// 			$('#navbar').stop().removeClass('scroll');
+//   		$('#nav-bar').stop().removeClass('scroll');
+//   		$('#pcf-logo').stop().removeClass('scroll');
+// 		}, else{
+// 			$('#navbar').stop().addClass('scroll');
+//   		$('#nav-bar').stop().addClass('scroll');
+//   		$('#pcf-logo').stop().addClass('scroll');
+// 		}
+  	
+// 	}
+// );
+
 $(function() {
     $('#navm a').bind('click',function(event){
       var $anchor = $(this);
